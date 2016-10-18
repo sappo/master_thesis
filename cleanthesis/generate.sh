45944868140733fd1c9792d0c261eeea3fd1e9fd
Kevin Sapper <mail@kevinsapper.de>
2016-10-18 22:44:42 +0200

    Added cleanthesis with pandoc template

---
+++ cleanthesis/generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#pandoc -s -V geometry="paperwidth=18cm, paperheight=34cm, margin=0.3cm" \
+       #-V lang=de-DE --highlight-style=espresso --filter=pandoc-citeproc \
+       #--biblio=paper.bib --csl=ieee.csl \
+       #-o paper.pdf paper.md
+pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
+       --filter=pandoc-crossref -M "crossrefYaml=./pandoc-crossref-de.yaml" \
+       --filter=pandoc-citeproc \
+       --latex-engine=xelatex --template=thesis-example.tex \
+       -o ./expose_thesis.pdf ./expose_thesis.md
