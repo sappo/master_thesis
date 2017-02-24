c26984015bd16ab0fa9b44c38619f908beeccabd
Kevin Sapper <mail@kevinsapper.de>
2017-02-24 13:12:45 +0100

    Almost reworked chapter 2

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
-       --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
-       --filter=pandoc-citeproc \
-       --latex-engine=xelatex --template=thesis_template.tex \
-       -o ./thesis.pdf ./thesis.md ./content/ch*.md
+if [ "$1" == '--tex' ]; then
+    pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
+           --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
+           --filter=pandoc-citeproc \
+           --latex-engine=xelatex --template=thesis_template.tex \
+           -o ./thesis.tex ./thesis.md ./content/ch*.md
+else
+    pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
+           --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
+           --filter=pandoc-citeproc \
+           --latex-engine=xelatex --template=thesis_template.tex \
+           -o ./thesis.pdf ./thesis.md ./content/ch*.md
+fi
