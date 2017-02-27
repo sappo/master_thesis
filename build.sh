b7c853bf4402be0803474a1fe184087fecaa775c
Kevin Sapper <mail@kevinsapper.de>
2017-02-27 11:13:13 +0100

    Added pandoc filter to process plantuml

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+export PATH=$PATH:$PWD/tools/
+
 if [ "$1" == '--tex' ]; then
     pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
            --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
            --filter=pandoc-citeproc \
+           --filter=./filters/plantuml.py \
            --latex-engine=xelatex --template=thesis_template.tex \
            -o ./thesis.tex ./thesis.md ./content/ch*.md
 else
     pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
            --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
            --filter=pandoc-citeproc \
+           --filter=./filters/plantuml.py \
            --latex-engine=xelatex --template=thesis_template.tex \
            -o ./thesis.pdf ./thesis.md ./content/ch*.md
 fi
