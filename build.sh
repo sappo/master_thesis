c65182bd345bead4219185140195718350a3152e
Kevin Sapper <mail@kevinsapper.de>
2017-03-04 16:16:59 +0100

    Add new filters

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export PATH=$PATH:$PWD/tools/
+export PATH=$PATH:$PWD/tools/asciitosvg/
 
 if [ "$1" == '--tex' ]; then
     pandoc --number-sections -V lang=de-DE --highlight-style=espresso \
            --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
            --filter=pandoc-citeproc \
            --filter=./filters/plantuml.py \
+           --filter=./filters/ascii2svg.py \
+           --filter=./filters/latex_blocks.py \
            --latex-engine=xelatex --template=thesis_template.tex \
            -o ./thesis.tex ./thesis.md ./content/ch*.md
 else
@@ -13,6 +16,8 @@ else
            --filter=pandoc-crossref -M "crossrefYaml=pandoc-crossref-de.yaml" \
            --filter=pandoc-citeproc \
            --filter=./filters/plantuml.py \
+           --filter=./filters/ascii2svg.py \
+           --filter=./filters/latex_blocks.py \
            --latex-engine=xelatex --template=thesis_template.tex \
            -o ./thesis.pdf ./thesis.md ./content/ch*.md
 fi
